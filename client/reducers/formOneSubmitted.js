7d340d3e773851639339bd6c27ed91425980c0ed
Danny Paton <danny.paton@gmail.com>
2019-02-27 11:51:40 -0500

    updates to reducers initial state

--- client/reducers/formOneSubmitted.js
+++ client/reducers/formOneSubmitted.js
@@ -1,8 +1,14 @@
-function formOneSubmitted(state = [], action) {
-  switch(action.type) {
+// default state variable
+const initialState = {
+  firstName: '',
+  lastName: ''
+}
+
+function formOneSubmitted(state = initialState, action) {
+  switch (action.type) {
     case 'FORMONE_SUBMIT':
-      // return the new state with the new comment
-      return [...state,{
+      // return the new state with the new submitted data
+      return [...state, {
         firstName: action.firstName,
         lastName: action.lastName
       }];
--- client/reducers/formTwoSubmitted.js
+++ client/reducers/formTwoSubmitted.js
@@ -1,8 +1,14 @@
-function formTwoSubmitted(state = [], action) {
-  switch(action.type) {
+// default state variable
+const initialState = {
+  firstName: '',
+  lastName: ''
+}
+
+function formTwoSubmitted(state = initialState, action) {
+  switch (action.type) {
     case 'FORMTWO_SUBMIT':
-      // return the new state with the new comment
-      return [...state,{
+      // return the new state with the new submitted data
+      return [...state, {
         firstName: action.firstName,
         lastName: action.lastName
       }];
